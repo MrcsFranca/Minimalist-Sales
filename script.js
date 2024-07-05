76c376be28de0e143ab92a2efcd17bd8086f7156
MrcsFranca <francamarcos035@gmail.com>
2024-07-05 18:47:29 -0300

    Adding the api code

--- script.js
+++ script.js
@@ -1,42 +1,36 @@
-// Search
-let input = document.getElementById('search-bar').value
-input = input.toLowerCase()
+const fetchProducts = async (query) => {
+	const response = await fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${query}`);
+	const data = await response.json();
+	return data.results;
+  };
+  const fetchByID = async(id)=>{
+	const response = await fetch(`https://api.mercadolibre.com/items/${id}`);
+	const data = await response.json();
+	console.log(data)
+	return data[0].body;
+  }
+  export default fetchProducts;
+  export {fetchByID};
+  
+  console.log(fetchProducts('celular'))
+  console.log(fetchByID('MLB3497242371'))
 
-
-// Variables
-const controls = document.querySelectorAll('.control')
-let currentItem = 0;
-const items = document.querySelectorAll('.item')
-let maxItems = items.length
-
-// Clique nas setas
-controls.forEach((control) => {
-	control.addEventListener('click', () => {
-		const isLeft = control.classList.contains('arrow-left')
-
-		if (isLeft) {
-			currentItem -= 1
-		} else {
-			currentItem += 1
-		}
-
-		if (currentItem >= maxItems) {
-			currentItem = 0
-		}
-		if (currentItem < 0) {
-			currentItem = maxItems - 1
-		}
-
-		// Trocar a imagem atual
-		items.forEach((item) => item.classList.remove('current-item'))
-
-		items[currentItem].scrollIntoView({
-			inline: 'center',
-			behavior: 'smooth',
-		})
-
-		items[currentItem].classList.add('current-item')
-
-		console.log
-	})
-})
+// fetch('', options)
+// 	.then(res => res.json())
+// 	.then((json) => {
+// 		const ul = document.getElementById('productList')
+// 		// json.forEach(() => {
+// 		// 	const li = document.createElement("div.container-fluid.object")
+// 		// 	li.innerHTML = `
+// 		// 		<div class="card" style="width: 18rem;">
+// 		// 			<img src="${}">
+// 		// 				<h5 class="card-title">${}</h5>
+// 		// 				<p class="card-text">${}</p>
+// 		// 				<a href="${}" class="btn btn-primary">Veja o produto no site</a>
+// 		// 			</div>
+// 		// 		</div>
+// 		// 	`
+// 		// })
+// 		console.log(json)
+// 		})
+// 	// })
