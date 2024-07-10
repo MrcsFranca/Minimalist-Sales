cdfd1b53797089b78b901d06bc6793fbe92284cd
Ruanrpm <ruan.pablo.martinz@gmail.com>
2024-07-09 21:52:38 -0300

    filter search-bar

--- script.js
+++ script.js
@@ -11,13 +11,13 @@
 //   }
 //   export default fetchProducts;
 //   export {fetchByID};
-  
+
 //   console.log(fetchProducts('celular'))
 //   console.log(fetchByID('MLB3740141311'))
 
-  fetch('https://api.mercadolibre.com/sites/MLB/search?q=${query}')
-  	.then(res => res.json())
-  	.then((json) => {
+fetch('https://api.mercadolibre.com/sites/MLB/search?q=${query}')
+	.then(res => res.json())
+	.then((json) => {
 		console.log(json);
 		console.log(json.results);
 		const ul = document.getElementById('productList')
@@ -36,6 +36,60 @@
 			ul.appendChild(li)
 		})
 	})
+export function filtrar() {
+	var input,
+		filter,
+		ul,
+		li,
+		a,
+		i,
+		txtValue,
+		cout = 0,
+		span
+
+	// pegar elementos
+	input = document.getElementById('search-bar');
+	ul = document.getElementById('productList');
+
+	// filtro
+	filter = input.value.toUpperCase();
+
+	// pegar LI´s da lista
+	li = ul.getElementsByTagName("li");
+
+	console.log(li);
+
+	//percorrer li
+
+	for (i = 0; i < li.length; i++) {
+		a = li[i].getElementsByTagName("a")[0];
+
+		txtValue = a.textContent || a.innerText;
+
+		// verifica se o valor que o usuario digitou é correto
+		if (txtValue.toUpperCase().indexOf(filter) > - 1) {
+			// valor bateu
+			li[i].style.display = "";
+			cout++
+			span = li[i].querySelector(".item-title");
+
+			if (span) {
+				span.innerHTML = txtValue.replace(new RegExp(filter, "gi"), (match) => {
+					return "<strong>" + match + "</strong>";
+				})
+			}
+		}
+		else {
+			//não mostra o item da lista
+			li[i].style.display = "none";
+		}
+	}
+}
+
+
+
+
+
 
 // fetch('', options)
 // 	.then(res => res.json())
