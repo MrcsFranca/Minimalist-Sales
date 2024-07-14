99b3d1f2fd8768600be10deedacc0f4333671d41
MrcsFranca <francamarcos035@gmail.com>
2024-07-14 15:33:12 -0300

    Customizing somethings

--- script.js
+++ script.js
@@ -1,29 +1,31 @@
 const form = document.getElementById('form')
 const searchInput = document.getElementById('search-bar')
 
+var searched = document.getElementById('searched')
+var searchResult = document.createElement('div')
+
+searchResult.innerHTML = '<h4>Faça uma pesquisa para ver os resultados</h4>'
+searched.appendChild(searchResult)
 
 
 form.addEventListener("submit", (submission) => {
 	submission.preventDefault()
 	
-	console.log(searchInput.value)
-
-	const searched = document.getElementById('searched')
-	const title = document.createElement('div')
-	title.innerHTML = `<h4>Resultado de: ${searchInput.value}</h4>`
-	searched.appendChild(title)
+	const ul = document.getElementById('productList')
+	let li = document.createElement("div.container-fluid")
+	
+	searchResult.innerHTML = `<h4>Resultado de: ${searchInput.value}</h4>`
+	
+	searched.appendChild(searchResult)
 
 	fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${searchInput.value}`)
 	.then(res => res.json())
 	.then((json) => {
-		console.log(json);
-		console.log(json.results);
-		const ul = document.getElementById('productList')
 		json.results.forEach((item) => {
-			const li = document.createElement("div.container-fluid")
+			li = document.createElement("div.container-fluid")
 			li.innerHTML = `
 			<div class="card" style="width: 18rem;">
-  				<img src="${item.thumbnail}" class="card-img-top" alt="...">
+  				<img src="${item.thumbnail}" class="card-img-top" style="max-width: 190px; align-self: center;" alt="...">
   				<div class="card-body">
     				<h5 class="card-title">${item.title}</h5>
 					<p class="card-text">R$${item.price}</p>
@@ -34,6 +36,7 @@ form.addEventListener("submit", (submission) => {
 			ul.appendChild(li)
 		})
 	})
+	searchInput.value = ""
 })
 
 
